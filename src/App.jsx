06499f75640e4b56b9313594e3a367de876c1ac4
Luiz Henrique <luizhenriquesouza866@icloud.com>
2024-12-12 20:31:19 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState , useEffect } from 'react'
+
+import './App.css'
+import Cronometro from './components/Cronometro'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Cronometro/>
+      </div>
+    </>
+  )
+}
+
+export default App  
